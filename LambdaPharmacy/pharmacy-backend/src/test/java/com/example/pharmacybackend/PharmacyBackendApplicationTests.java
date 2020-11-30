f1a5c7e23b13cd51eaa158483d122ec126b71e18
ivanatomic43 <ivana.tomic96@gmail.com>
2020-11-30 14:37:10 +0100

    Initializing project

---
+++ LambdaPharmacy/pharmacy-backend/src/test/java/com/example/pharmacybackend/PharmacyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmacybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
